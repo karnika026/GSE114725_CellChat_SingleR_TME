e9b23f58d439b409510b81f600a2ab1a389c17d4
karnika026 <52640988+karnika026@users.noreply.github.com>
2026-03-15 23:23:03 -0400

    Update comment syntax for aim description

--- Visualization.R
+++ Visualization.R
@@ -1,5 +1,5 @@
 
-Aim: To Generate CellChat communication network visualizations
+#Aim: To Generate CellChat communication network visualizations
 
 # -------------------------------
 # Load libraries
